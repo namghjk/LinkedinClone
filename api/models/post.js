762fb69af09a971458c0b65d56e6943cbac4e3a1
namghjk <namsgu123@gmail.com>
2023-11-11 15:54:51 +0700

    finish UI LoginScreen

--- api/models/post.js
+++ api/models/post.js
@@ -1,5 +1,36 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-    
-})
+  description: String,
+  imageURL: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      text: String,
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+});
+
+const Posts = mongoose.model('Posts', postSchema);
+
+module.exports = Posts;
---
+++ app/(authenticate)/register.js
@@ -0,0 +1,14 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const register = () => {
+  return (
+    <SafeAreaView>
+      <Text>register</Text>
+    </SafeAreaView>
+  );
+};
+
+export default register;
+
+const styles = StyleSheet.create({});
